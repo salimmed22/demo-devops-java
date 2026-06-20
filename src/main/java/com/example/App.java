82388b038549be63671737ca485c3d1826013d31
Mederbel Salim <mederbelsalim@gmail.com>
2026-06-20 23:26:07 +0200

    Add sum method and JUnit test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,11 @@
 package com.example;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,19 +1,12 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testSum() {
+        int result = App.sum(2, 3);
+        assertEquals(5, result, "2 + 3 should be 5");
     }
 }
